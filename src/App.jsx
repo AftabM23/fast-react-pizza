f5c12050d754dc4816b757ade374f516912bc8a5
AftabM23 <aftaba.mohammed23@gmail.com>
2024-09-04 17:20:39 -0400

    installed react-router-dom and implemented basic routing using creaetBrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import CreateOrder from "./features/order/CreateOrder";
+import OrderItem from "./features/order/OrderItem";
+import Cart from "./features/cart/Cart";
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/menu", element: <Menu /> },
+  { path: "/cart", element: <Cart /> },
+  { path: "/order/new", element: <CreateOrder /> },
+  { path: "/order/:id", element: <OrderItem /> },
+]);
+
 function App() {
-  return (
-    <>
-      <h1>hi</h1>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
