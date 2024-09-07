f6d0bdddab20abd758462c61f12c29d3fc71b619
AftabM23 <aftaba.mohammed23@gmail.com>
2024-09-06 22:35:12 -0400

    styled loader

--- src/ui/LoadingComponent.jsx
+++ src/ui/LoadingComponent.jsx
@@ -1,15 +1,17 @@
-import styles from "./LoadingComponent.module.css";
+import styles from './LoadingComponent.module.css';
 function LoadingComponent() {
   return (
-    <div className={styles.dotSpinner}>
-      <div className={styles.dotSpinnerDot}></div>
-      <div className={styles.dotSpinnerDot}></div>
-      <div className={styles.dotSpinnerDot}></div>
-      <div className={styles.dotSpinnerDot}></div>
-      <div className={styles.dotSpinnerDot}></div>
-      <div className={styles.dotSpinnerDot}></div>
-      <div className={styles.dotSpinnerDot}></div>
-      <div className={styles.dotSpinnerDot}></div>
+    <div className="absolute inset-0 z-30 flex items-center justify-center bg-slate-200/50 backdrop-blur-sm">
+      <div className={styles.dotSpinner}>
+        <div className={styles.dotSpinnerDot}></div>
+        <div className={styles.dotSpinnerDot}></div>
+        <div className={styles.dotSpinnerDot}></div>
+        <div className={styles.dotSpinnerDot}></div>
+        <div className={styles.dotSpinnerDot}></div>
+        <div className={styles.dotSpinnerDot}></div>
+        <div className={styles.dotSpinnerDot}></div>
+        <div className={styles.dotSpinnerDot}></div>
+      </div>
     </div>
   );
 }
