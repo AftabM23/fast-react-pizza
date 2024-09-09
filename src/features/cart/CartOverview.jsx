2084cb1e769a7c01664b3f36d28140af0d2ecfdd
AftabM23 <aftaba.mohammed23@gmail.com>
2024-09-08 22:47:55 -0400

    implemented cartOverview by calculating using reduce

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,11 +1,26 @@
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 function CartOverview() {
+  const totalPrice = useSelector((state) =>
+    state.cart.cart.reduce(
+      (total, item) => total + item.quantity * item.unitPrice,
+      0,
+    ),
+  );
+
+  const totalPizzas = useSelector((state) =>
+    state.cart.cart.reduce(
+      (totalPizzas, item) => totalPizzas + item.quantity,
+      0,
+    ),
+  );
+
   return (
     <div className="flex justify-between bg-stone-800 p-2 text-center uppercase text-stone-200 md:p-4">
       <p className="space-x-5">
-        <span>23 pizzas</span>
-        <span>$23.45</span>
+        <span>{totalPizzas} pizzas</span>
+        <span>${totalPrice}</span>
       </p>
       <Link to="/cart">Open cart &rarr;</Link>
     </div>
